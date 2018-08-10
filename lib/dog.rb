12e5b927bd88b4c2ccc3ac67a6f023c95255e0d0
Nikki A <nikki.apostolakis@gmail.com>
2018-08-10 21:55:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,7 +34,7 @@ class Dog
       SQL
 
       DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
   end
   def update
